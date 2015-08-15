a875183fa3699c98bc4f8f85ee404d6dfaceadd6
Buck Shlegeris <bshlegeris@paypal.com>
2015-08-15 16:24:20 -0700

    more stuff

--- make_lists.rb
+++ make_lists.rb
@@ -3,6 +3,6 @@ var allFiles = #{`find .`.split("\n")};
 var allSubdirectories = #{`find . -type d`.split("\n")};
 JAVASCRIPT
 
-File.open("file_data.js", "w") do |f|
+File.open('file_data.js', 'w') do |f|
   f.write(json_file)
 end
